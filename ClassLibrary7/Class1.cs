ff9a9a69f23eb065aba34d0e00150c62752c29f6
Gajendra <gajendra.ranjith@gmail.com>
2019-09-09 14:58:10 +0530

    Add project files.

---
+++ ClassLibrary7/Class1.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace ClassLibrary7
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void tcLogin()
+        {
+            string url = "http://172.16.51.121/LMSPublish/Login.aspx";
+            string Username = "admin";
+            string Password = "admin@123";
+            
+            IWebDriver oWD = new ChromeDriver();
+            oWD.Navigate().GoToUrl(url);
+            oWD.FindElement(By.Id("inputEmail")).SendKeys(Username);
+            oWD.FindElement(By.Id("inputPassword")).SendKeys(Password);
+            oWD.FindElement(By.Id("btnLogin")).Click();
+
+            //oWD.Close();
+        }
+
+        [Test]
+        public void tcNewMember()
+        {
+
+        }
+    }
+}
